0f596e80288e8c0b61f7b4f5c34b1dea26565cba
gorkemakpur <gorkemakpur@gmail.com>
2023-11-12 02:55:26 +0300

    ders-4

---
+++ OOP-1/ProductManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_1
+{
+    public class ProductManager
+    {
+        public void Add(Product product)
+        {
+            product.ProductName = "Kamera";
+        }
+        public void BiseyYap(int sayi)
+        {
+            sayi = 99;
+        }
+
+    }
+}
---
+++ OOP-1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace OOP_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 1;
+            product1.ProductName = "Test";
+            product1.Price = 10;
+            product1.UnitsInStock = 5;
+
+            Product product2 = new Product()
+            {
+                Id = 2,
+                CategoryId = 1,
+                ProductName = "Test-2-",
+                Price = 10,
+                UnitsInStock = 5
+            };
+            //PascalCase  //camelCase  case sensitive
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            int sayi = 100;
+            productManager.BiseyYap(sayi);
+            Console.WriteLine(sayi);
+
+            Console.WriteLine(product1.ProductName);
+        }
+    }
+}
