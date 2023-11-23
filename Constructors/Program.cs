c71f13fe11460cb5a9e9a0c32664e1c48e4a89b6
gorkemakpur <gorkemakpur@gmail.com>
2023-11-23 19:05:25 +0300

    Constructor

---
+++ Constructors/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer(1,"test","test","test");
+            Customer customer2 = new Customer { Id=2,FirstName="2test",LastName="2test",City="2test"};
+        }
+    
+    }
+
+    class Customer
+    {
+        public Customer()
+        {
+            
+        }
+        public Customer(int id, string firstName, string lastName, string city)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
