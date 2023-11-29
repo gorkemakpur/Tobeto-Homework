b5fa97bf162c32645c0bcba75e82a4c2f010f9d6
gorkemakpur <gorkemakpur@gmail.com>
2023-11-29 14:53:43 +0300

    Interfaces

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -9,14 +9,19 @@
             sayi1 = sayi2;
             sayi2 = 65;
 
-            Console.WriteLine(sayi1);
+            //Console.WriteLine(sayi1);
 
             int[] sayilar1 = new int[] { 10, 20, 30 };
             int[] sayilar2 = new int[] { 50, 60, 70 };
             sayilar1 = sayilar2;
             sayilar2[0] = 30000;
 
-            Console.WriteLine(sayilar1[0]);
+            //Console.WriteLine(sayilar1[0]);
+
+
+            //Console.WriteLine(typeof(string).IsPrimitive);
+            //Console.WriteLine(typeof(int).IsPrimitive);
+
 
         }
     }
---
+++ Interfaces/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Security.Cryptography;
+
+namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPersonManager customerManager = new CustomerManager();
+            customerManager.Add();
+            IPersonManager employeeManager = new EmployeeManager();
+            employeeManager.Add();
+        }
+
+        interface IPersonManager
+        {
+            //unimplement operation
+            void Add();
+            void Update();
+        }
+
+        class CustomerManager : IPersonManager
+        {
+            public void Add()
+            {
+                Console.WriteLine("Added");
+            }
+
+            public void Update()
+            {
+                Console.WriteLine("Updated");
+            }
+        }
+
+        class EmployeeManager : IPersonManager
+        {
+            public void Add()
+            {
+                Console.WriteLine("Added");
+            }
+
+            public void Update()
+            {
+                Console.WriteLine("Updated");
+            }
+        }
+
+        class ProjectManager
+        {
+            public void Add(IPersonManager personManager)
+            {
+                personManager.Add();
+            }
+        }
+    }
+}
